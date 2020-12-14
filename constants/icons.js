5230b9613c65879f1a0a0f9d56616e5f541c7a2b
Yanuardi <52162301+zulhwk@users.noreply.github.com>
2020-12-14 11:34:56 +0700

    constant and assets installing

---
+++ constants/icons.js
@@ -0,0 +1,17 @@
+export const airplane = require("../assets/icons/airplane_icon.png");
+export const back = require("../assets/icons/back_icon.png");
+export const barMenu = require("../assets/icons/bar_menu_icon.png");
+export const bed = require("../assets/icons/bed_icon.png");
+export const bookmark = require("../assets/icons/bookmark_icon.png");
+export const bus = require("../assets/icons/bus_icon.png");
+export const compass = require("../assets/icons/compass_icon.png");
+
+export default {
+  airplane,
+  back,
+  barMenu,
+  bed,
+  bookmark,
+  bus,
+  compass
+};
---
+++ constants/images.js
@@ -0,0 +1,11 @@
+export const beach = require("../assets/images/beach.jpeg");
+export const climbing = require("../assets/images/climbing_hills.jpg");
+export const frozen = require("../assets/images/frozen_hills.jpg");
+export const home = require("../assets/images/home_banner.jpg");
+
+export default {
+  beach,
+  climbing,
+  frozen,
+  home
+};
